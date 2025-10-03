b944d7c8570ea4f250bf537d40b689ff46dbd5f1
Jules <jbjulesbernard3@gmail.com>
2025-10-03 17:23:23 +0200

    fix message class added with construction and validation draft

--- common/src/main/java/com/github/julesrb/fixme/common/FixMessage.java
+++ common/src/main/java/com/github/julesrb/fixme/common/FixMessage.java
@@ -1,4 +1,119 @@
 package com.github.julesrb.fixme.common;
 
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Integer.parseUnsignedInt;
+
+// support only FIX 4.4
 public class FixMessage {
+
+    private String msg;
+
+    private Map<Integer, String> fields = new HashMap<>();
+
+    public static final int BEGIN_STRING = 8;
+    public static final int MSG_TYPE = 35;
+    public static final int SENDER_COMP_ID = 49; // ID Broker
+    public static final int TARGET_COMP_ID = 56; // Market
+    public static final int INSTRUMENT = 55;
+    public static final int SIDE = 54;
+    public static final int QUANTITY = 38;
+    public static final int PRICE = 44;
+    public static final int CHECKSUM = 10;
+
+    private final int[] expectedTags = {8, 35, 49, 56, 55, 54, 38, 44};
+
+
+    public FixMessage(String message) {
+        this.msg = message;
+        String[] tokens = message.split("\u0001");
+        for (String token : tokens) {
+            if (token.isEmpty()) continue;
+            String[] part = token.split("=", 2);
+            int tag = parseInt(part[0]);
+            fields.put(tag, part[1]);
+            System.out.println("added " + part[0] + " = " + part[1]);
+        }
+    }
+
+    private Boolean isUnsignedInt(String str) {
+        try {
+            int id = parseUnsignedInt(str);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    //Check if the fields are correct, not empty and that the minimum fields are present
+    public Boolean validateMessage() {
+        if (!validateChecksum())
+            throw new RuntimeException("Checksum incorrect");
+        for (int key : expectedTags) {
+            if (!fields.containsKey(key)) throw new RuntimeException("Missing tag = " + key);
+        }
+        for (Map.Entry<Integer,String> field : fields.entrySet()) {
+            int key = field.getKey();
+            String value = field.getValue();
+//            if (value.isBlank()) throw new RuntimeException("Value missing for key " + key);
+            switch (key) {
+                case BEGIN_STRING:
+                    if (!value.equals("FIX.4.4")) throw new RuntimeException("Expect FIX version 4.4");
+                    break;
+                case MSG_TYPE:
+                    if (value.length() > 2) throw new RuntimeException("Message type unknown");
+                    break;
+                case SENDER_COMP_ID:
+                    if (!isUnsignedInt(value)) throw new RuntimeException("Broker ID type error");
+                    break;
+                case TARGET_COMP_ID:
+                    if (!isUnsignedInt(value)) throw new RuntimeException("Market ID type error");
+                    break;
+                case INSTRUMENT:
+                    //In a simplified project, just define a list of strings that your Market supports, e.g.:
+                    //If a Broker sends an order for a symbol not in the list, the Market should reject it.//
+                    // List<String> tradableInstruments = List.of("AAPL", "GOOG", "TSLA");
+                    //207 = SecurityExchange → exchange where the symbol trades (NASDAQ, NYSE)
+	                //48 = SecurityID → unique identifier (ISIN, CUSIP)
+	                //22 =SecurityIDSource → source of SecurityID (e.g., ISIN)
+                    break;
+                case SIDE:
+                    if (!(value.equals("1") || value.equals("2"))) throw new RuntimeException("Wrong side argument");
+                    break;
+                case QUANTITY:
+                    if (!isUnsignedInt(value)) throw new RuntimeException("Quantity type error");
+                    break;
+                case PRICE:
+                    if (!isUnsignedInt(value)) throw new RuntimeException("Price ID type error");
+                    break;
+            }
+        }
+        return true;
+    }
+
+    public String calculateChecksum(String messageWithoutChecksum) {
+        byte[] bytes = messageWithoutChecksum.getBytes(StandardCharsets.US_ASCII);
+        int sum = 0;
+        for (byte b : bytes) {
+            sum += (b & 0xFF); // Makes it unsigned
+        }
+        int checksum = sum % 256;
+        return String.format("%03d", checksum); // always 3 digits
+    }
+
+    public Boolean validateChecksum() {
+        int lastTagIndex = msg.lastIndexOf("10=");
+        if (lastTagIndex == -1) return false;
+
+        String messageWithoutChecksum = msg.substring(0, lastTagIndex);
+        String provided = fields.get(CHECKSUM);
+        String expected = calculateChecksum(messageWithoutChecksum);
+
+        return expected.equals(provided);
+    }
+
 }
+
--- market/src/main/java/com/github/julesrb/fixme/market/Main.java
+++ market/src/main/java/com/github/julesrb/fixme/market/Main.java
@@ -1,11 +1,25 @@
 package com.github.julesrb.fixme.market;
 
+import com.github.julesrb.fixme.common.FixMessage;
+
+import java.math.BigDecimal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome from market!");
+        System.out.println("Hello and welcome from market!");
+        try {
+//            FixMessage msg = new FixMessage("8=FIX.4.4|35=D|49=BROKER01|56=MARKET01|34=1|11=ORDER123|55=AAPL|54=1|38=100|44=150.50|10=123|");
+//            msg.validateMessage();
+//            FixMessage msg2 = new FixMessage("8=FIX.4.4\u00019=65\u000135=D\u000134=1\u000149=BROKER01\u000156=MARKET01\u000111=ORDER123\u000155=AAPL\u000154=1\u000138=100\u000144=150.50\u000110=072\u0001");
+//            msg2.validateMessage();
+            FixMessage msg3 = new FixMessage("8=FIX.4.4\u00019=65\u000135=\u000134=1\u000149=BROKER01\u000156=MARKET01\u000111=ORDER123\u000155=AAPL\u000154=1\u000138=100\u000144=150.50\u000110=072\u0001");
+            msg3.validateMessage();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
